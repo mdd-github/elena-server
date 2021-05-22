a77775157f4e703a8481ada2cc6e13f12ce6d397
Daniil Mitrofanov <mdd-inbox@ya.ru>
2021-05-23 01:26:48 +0200

    Project configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('TYPEORM_HOST'),
+        port: configService.get<number>('TYPEORM_PORT'),
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        entities: [configService.get<string>('TYPEORM_ENTITIES')],
+        synchronize: false,
+      }),
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
+const parseCorsWhitelist = (list: string): string[] => {
+  return list != null ? list.split(';') : ['http://localhost:3000'];
+};
+
+const applicationPort = process.env.APP_PORT || 5000;
+const corsWhiteList = parseCorsWhitelist(process.env.APP_CORS_WHITELIST);
+
+const applyMiddlewares = (app: INestApplication) => {
+  app.use(cookieParser());
+};
+
+const enableCors = (app: INestApplication) => {
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (corsWhiteList.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        Logger.log('blocked cors for:' + origin, '[CORS]');
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+};
+
+(async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  enableCors(app);
+  applyMiddlewares(app);
+  await app.listen(applicationPort);
+})();
